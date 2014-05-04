1a6e9fa827fbaf70b86b6f617c292930d466584c
Githubshanna <shannahutch@gmail.com>
2014-05-04 14:17:54 -0700

    First commit on geonote app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geonote::Application.config.secret_key_base = '671f1912fa26f4cee3d443bc4a0a4ecac5124299d252bec3ea11cc17d73f4e4f01d409787493644759720366afcafaac1ed0ad17ce15c058e8f4959222c431c4'
