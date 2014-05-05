bcdf14cb6eaa69e78dd6ca89aa98fc47095710ef
Githubshanna <shannahutch@gmail.com>
2014-05-05 15:28:14 -0700

    added note controller with index and new, and routes.draw

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,7 @@
+class NotesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,31 @@
 Geonote::Application.routes.draw do
+  get "notes/index"
+  get "notes/new"
   devise_for :users
 
+  root 'notes#new'
+
+  get '/notes/new', to: 'notes#new', as: 'notes_new'
+  post 'notes', to: 'notes#create', as: 'notes'
+  # get '/notes/:id', to: 'notes#show', as: 'notes'
   
 end
+
+#  Prefix Verb   URI Pattern                    Controller#Action
+#         new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
+#             user_session POST   /users/sign_in(.:format)       devise/sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
+#            user_password POST   /users/password(.:format)      devise/passwords#create
+#        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+#                          PATCH  /users/password(.:format)      devise/passwords#update
+#                          PUT    /users/password(.:format)      devise/passwords#update
+# cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
+#        user_registration POST   /users(.:format)               devise/registrations#create
+#    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
+#   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
+#                          PATCH  /users(.:format)               devise/registrations#update
+#                          PUT    /users(.:format)               devise/registrations#update
+#                          DELETE /users(.:format)               devise/registrations#destroy
+#                     root GET    /                              notes#new
+#                notes_new GET    /notes/new(.:format)           notes#new
