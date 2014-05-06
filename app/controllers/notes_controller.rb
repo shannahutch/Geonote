6f0d1db3858b3c50ae9e9fbd9bca9bcbf4ed15f7
Githubshanna <shannahutch@gmail.com>
2014-05-06 15:51:04 -0700

    fixed user object shown to user email

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -14,6 +14,8 @@ class NotesController < ApplicationController
   end
 
   def create
+    # binding.pry
+    
     @note = Note.new note_params
     if @note.save
       redirect_to note_path(@note)
