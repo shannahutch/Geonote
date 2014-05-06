16d02600f6c804b5488b5f525840cb31bd570c1b
Githubshanna <shannahutch@gmail.com>
2014-05-06 15:34:23 -0700

    changed lat long to floats

---
+++ db/migrate/20140506222842_change_lat_long_to_float.rb
@@ -0,0 +1,8 @@
+class ChangeLatLongToFloat < ActiveRecord::Migration
+  def change
+    remove_column :notes, :latitude
+    remove_column :notes, :longitude
+    add_column :notes, :latitude, :float
+    add_column :notes, :longitude, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505211503) do
+ActiveRecord::Schema.define(version: 20140506222842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "notes", force: true do |t|
-    t.string   "latitude"
-    t.string   "longitude"
     t.string   "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "users", force: true do |t|
