74cc4ab70627dddee32f3738b379335c3a71f6ec
Githubshanna <shannahutch@gmail.com>
2014-05-05 17:42:09 -0700

    updated note_controller, htmls, added methods

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,45 @@
 class NotesController < ApplicationController
   def index
+    @notes = Note.all
+  end
+
+  def show  
+    @note = Note.find(params[:id])
+
   end
 
   def new
+    @note = Note.new
+    
   end
+
+  def create
+    note = Note.new note_params
+    if note.save
+      redirect_to note(@note)
+    else
+      flash[:error] = "Your note was not saved. Try again"
+      redirect_to root_path
+    end
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    @note.update note_params
+    redirect_to note(@note)
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to root
+  end
+
+  private
+    def note_params
+      params.require(:note).permit(:text)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Geonote::Application.routes.draw do
-  get "notes/index"
-  get "notes/new"
+  
   devise_for :users
 
   root 'notes#new'
 
-  get '/notes/new', to: 'notes#new', as: 'notes_new'
-  post 'notes', to: 'notes#create', as: 'notes'
-  # get '/notes/:id', to: 'notes#show', as: 'notes'
+  resources :notes
+
+  # get "/notes/index", to: 'notes#index', as: 'notes'
+  # get '/notes/new', to: 'notes#new', as: 'new_note'
+  # post '/notes', to: 'notes#create'
+  # get '/notes/:id', to: 'notes#show', as: 'note'
+
   
 end
 
-#  Prefix Verb   URI Pattern                    Controller#Action
+#                   Prefix Verb   URI Pattern                    Controller#Action
 #         new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
 #             user_session POST   /users/sign_in(.:format)       devise/sessions#create
 #     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
@@ -28,4 +31,19 @@ end
 #                          PUT    /users(.:format)               devise/registrations#update
 #                          DELETE /users(.:format)               devise/registrations#destroy
 #                     root GET    /                              notes#new
-#                notes_new GET    /notes/new(.:format)           notes#new
+
+#                    notes GET    /notes/index(.:format)         notes#index
+#                 new_note GET    /notes/new(.:format)           notes#new
+#                          POST   /notes(.:format)               notes#create
+#                     note GET    /notes/:id(.:format)           notes#show
+
+# from resources :notes
+               #     notes GET    /notes(.:format)               notes#index
+               #           POST   /notes(.:format)               notes#create
+               #  new_note GET    /notes/new(.:format)           notes#new
+               # edit_note GET    /notes/:id/edit(.:format)      notes#edit
+               #      note GET    /notes/:id(.:format)           notes#show
+               #           PATCH  /notes/:id(.:format)           notes#update
+               #           PUT    /notes/:id(.:format)           notes#update
+               #           DELETE /notes/:id(.:format)           notes#destroy
+
