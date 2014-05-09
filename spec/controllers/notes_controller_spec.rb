0f32ce36c131dfebc8d33715d89345a54c472001
Githubshanna <shannahutch@gmail.com>
2014-05-08 19:14:32 -0700

    added an rspec to notes_spec.rb and tested out CSS

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -2,18 +2,34 @@ require 'spec_helper'
 
 describe NotesController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
+      describe "GET 'index'" do
+        it "returns http success" do
+          get 'index'
+          response.should be_success
+        end
+      end
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
+      describe "GET 'new'" do
+        it "returns http success" do
+          get 'new'
+          response.should be_success
+        end
+      end
 
-end
+    end
+
+#   describe 'Get index' do
+#     before do
+#       u = User.create!()
+#     end
+#     it 'returns a 200 Ok status' do
+#       note = Note.create
+#       get :index
+#       expect(response.status).to eq(200)
+#     end
+#     it 'renders the index template' do
+#       get :index
+#       expect(response).to render_template("index")  
+#     end
+#   end
+# end
--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -1,1 +1,24 @@
 # each method for routes in method
+require 'spec_helper'
+# 200 status check
+  
+  
+
+  
+describe NotesController do
+  before do
+    u = User.create!(email: 'shannahutch@gmail.com', password: 'asdasdasd', password_confirmation: 'asdasdasd')
+    post user_session_path, :user => {:email => u.email, :password => u.password}
+  end
+  describe 'Get index' do
+    it 'returns a 200 Ok status' do
+      note = Note.create
+      get notes_path
+      expect(response.status).to eq(200)
+    end
+    it 'renders the index template' do
+      get notes_path
+      expect(response).to render_template("index")  
+    end
+  end
+end
